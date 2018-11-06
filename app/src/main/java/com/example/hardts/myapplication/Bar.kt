c3b93f56d4fe590f329bf7695f686baadff5cfdd
Hardt, Simon <simon.hardt@loewen.de>
2018-11-06 10:25:58 +0100

    initial commit

---
+++ app/src/main/java/com/example/hardts/myapplication/Bar.kt
@@ -0,0 +1,15 @@
+package com.example.hardts.myapplication
+
+open class Foo
+
+interface Bar {
+    val lazyLoadedString : String
+}
+
+class Test {
+    private val foo by lazy {
+        object : Foo(), Bar {
+            override val lazyLoadedString = "Demo"
+        }
+    }
+}
